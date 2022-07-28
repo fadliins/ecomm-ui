8e2bcc72ab30cbc93070c704323a4f119e5b39af
Fadli Insani <fadliinsan1997@gmail.com>
2022-07-28 13:17:40 +0700

    Add Slider

---
+++ src/data.js
@@ -0,0 +1,20 @@
+export const sliderItems = [
+    {
+      id: 1,
+      img: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/5/T/5T9R3PA-1_T1657693059.png",
+      title: "SUMMER SALE",
+      desc: "IT Experiences Made Better"
+    },
+    {
+      id: 2,
+      img: "https://id-media.apjonlinecdn.com/catalog/product/cache/b3b166914d87ce343d4dc5ec5117b502/4/9/498R8PA-1_T1657691361.png",
+      title: "AUTUMN COLLECTION",
+      desc: "Designed with precision"
+    },
+    {
+      id: 3,
+      img: "https://id-media.apjonlinecdn.com/catalog/product/cache/74c1057f7991b4edb2bc7bdaa94de933/4/8/48L39PA-1_T1657694035.png",
+      title: "Winter Love",
+      desc: "Made affordable for you"
+    },
+  ];
