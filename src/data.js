130c0177b474baed66f2c694097f5fe35062ec42
Fadli Insani <fadliinsan1997@gmail.com>
2022-08-16 01:08:53 +0700

    Add popular products

--- src/data.js
+++ src/data.js
@@ -33,3 +33,38 @@ export const sliderItems = [
       img: "https://static-ecapac.acer.com/media/wysiwyg/Eco-Friendly-Products-Aspire-Vero_4.jpg"
     },
   ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://dlcdnwebimgs.asus.com/gain/3986df1f-5315-4e44-9ce0-e618e6709b59/w185/fwebp",
+    },
+    {
+      id:2,
+      img:"https://dlcdnwebimgs.asus.com/gain/4d4070c5-a35f-4d1e-ae2b-1a4e02f7b197/w185/fwebp",
+    },
+    {
+      id:3,
+      img:"https://dlcdnwebimgs.asus.com/gain/04354aee-b26d-482b-ab14-7aca3898cf99/w185/fwebp",
+    },
+    {
+      id:4,
+      img:"https://dlcdnwebimgs.asus.com/gain/b344cc6d-7d5d-48c2-a1b1-9daee5b9d77f/w185/fwebp",
+    },
+    {
+      id:5,
+      img:"https://dlcdnwebimgs.asus.com/gain/f8639d76-40a7-4457-b46a-c38e47456d78/w185/fwebp",
+    },
+    {
+      id:6,
+      img:"https://dlcdnwebimgs.asus.com/gain/32fdad3d-c930-41aa-906c-2d4d6766c131/w185/fwebp",
+    },
+    {
+      id:7,
+      img:"https://www.asus.com/media/global/gallery/t0Nrwz5deBPCSmF4_setting_xxx_0_90_end_184.png",
+    },
+    {
+      id:8,
+      img:"https://dlcdnwebimgs.asus.com/gain/2e235c45-76a0-417e-9624-a7c4fd998834/w185/fwebp",
+    },
+  ]
